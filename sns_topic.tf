d37823c7a878ca31e128fa87d87eb2f95992234d
Guilherme Afonso Borchardt <guilhermeabor@hotmail.com.br>
2022-03-13 00:58:26 -0300

    recover password topic

--- sns_topic.tf
+++ sns_topic.tf
@@ -1,3 +1,7 @@
 resource "aws_sns_topic" "mailer-verification-email" {
   name = "mailer-verification-email"
 }
+
+resource "aws_sns_topic" "mailer-recover-password" {
+  name = "mailer-recover-password"
+}
